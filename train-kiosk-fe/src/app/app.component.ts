f4d2985f2386257ffb71c2d856c61211cf8189ed
Prosthius <ietvntpd@duck.com>
2023-01-18 10:34:42 +1100

    Added imports

--- train-kiosk-fe/src/app/app.component.ts
+++ train-kiosk-fe/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ClockComponent } from './clock/clock.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <div>
+      <app-clock></app-clock>
+    </div>
+  `,
 })
+
 export class AppComponent {
   title = 'Train Kiosk';
 }
--- train-kiosk-fe/src/app/app.module.ts
+++ train-kiosk-fe/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InboundTrainsComponent } from './inbound-trains/inbound-trains.component';
+import { OutboundTrainsComponent } from './outbound-trains/outbound-trains.component';
+import { ClockComponent } from './clock/clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InboundTrainsComponent,
+    OutboundTrainsComponent,
+    ClockComponent
   ],
   imports: [
     BrowserModule
