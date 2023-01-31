7498cf57cbc2819bcb0973a3994294800770ed27
Prosthius <mail@callumhopkins.au>
2023-01-31 13:55:42 +1100

    Updated outbound-trains styling to match inbound-trains component

--- src/app/inbound-trains/inbound-trains.component.ts
+++ src/app/inbound-trains/inbound-trains.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api-service/api.service';
 
 @Component({
   selector: 'app-inbound-trains',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./inbound-trains.component.css']
 })
 export class InboundTrainsComponent {
+  constructor(private api: ApiService) { }
 
+  ngOnInit() {
+    console.log(this.api.getData());
+
+    let html: string;
+    let htmlSegment: string = `City Loop`;
+
+    document.getElementById("line-1").innerHTML = htmlSegment;
+  }
 }
