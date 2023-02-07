3139bb9c736bec8fa952b8a52c889bbee5150af7
Prosthius <mail@callumhopkins.au>
2023-02-08 10:15:40 +1100

    Created helper service for station IDs

---
+++ src/app/helper/helper.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelperService {
+
+  constructor() { }
+
+  Directions = {
+    "1": "Alamein",
+    "2": "Belgrave",
+    "3": "Craigieburn",
+    "4": "Cranbourne",
+    "5": "Mernda",
+    "6": "Frankston",
+    "7": "Glen Waverley",
+    "8": "Hurstbridge",
+    "9": "Lilydale",
+    "11": "Pakenham",
+    "12": "Sandringham",
+    "13": "Stony Point",
+    "14": "Sunbury",
+    "15": "Upfield",
+    "16": "Werribee",
+    "17": "Williamstown",
+    "1482": "Showgrounds - Flemington Racecourse"
+  }  
+}
