95fafc90f2e273471d8854317c123b139c5643cd
Prosthius <mail@callumhopkins.au>
2023-02-08 10:26:21 +1100

    Implementing api

--- src/app/inbound-trains/inbound-trains.component.ts
+++ src/app/inbound-trains/inbound-trains.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api-service/api.service';
+import { HelperService } from '../helper/helper.service';
+import { catchError, filter, map } from 'rxjs';
+import { Departure } from '../departure';
+
 
 @Component({
   selector: 'app-inbound-trains',
   templateUrl: './inbound-trains.component.html',
   styleUrls: ['./inbound-trains.component.css']
 })
-export class InboundTrainsComponent {
+export class InboundTrainsComponent implements OnInit {
+  constructor(private api: ApiService, public helper: HelperService) { }
+  data: string;
+  cityLoopDepartures: Array<string> = [];
+  nextTrainFlinders: any;
+  nextTrainCityLoop: any;
 
+  async ngOnInit() {
+    try {
+      this.nextTrainFlinders = await this.api.getDestinationNameInbound(`"Flinders Street "`);
+    } catch (error) {
+      console.log(error);
+    }
+
+    let date = new Date();
+    let time: string = date.toLocaleTimeString();
+    console.log(time);
+
+    let currentTime: any = new Date().toLocaleTimeString
+    let timeToArrival = this.nextTrainFlinders[0].estimated_departure_utc - currentTime;
+    console.log(currentTime);
+
+    // let currentTime = new Date();
+    // let estimatedDeparture = new Date(this.nextTrainFlinders[0].estimated_departure_utc);
+    // let timeToArrival = estimatedDeparture.getTime() - currentTime.getTime();
+
+
+    //this.nextTrainCityLoop = await this.api.getDestinationNameInbound(`"Parliament "`) || await this.api.getDestinationNameInbound(`"Southern Cross "`);
+  }
 }
--- src/app/outbound-trains/outbound-trains.component.ts
+++ src/app/outbound-trains/outbound-trains.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api-service/api.service';
+import { HelperService } from '../helper/helper.service';
 
 @Component({
   selector: 'app-outbound-trains',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./outbound-trains.component.css']
 })
 export class OutboundTrainsComponent {
+  constructor(private api: ApiService, public helper: HelperService) { }
+
 
 }
