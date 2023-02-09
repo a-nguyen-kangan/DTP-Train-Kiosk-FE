0e2584f72b319ba3f064a21ab96c818bf68cb863
Prosthius <mail@callumhopkins.au>
2023-02-09 15:19:05 +1100

    Added TrainDirections enum

--- src/app/departure.ts
+++ src/app/departure.ts
@@ -18,3 +18,23 @@ export class NextDeparture {
     time: string;
     platformNumber: string;
 }
+
+export enum Directions {
+    "Alamein " = 1,
+    "Belgrave " = 2,
+    // Craigieburn = 3,
+    "Cranbourne " = 4,
+    // Mernda = 5,
+    "Frankston " = 6,
+    "Glen Waverley " = 7,
+    // Hurstbridge = 8,
+    "Lilydale "= 9,
+    "Pakenham " = 11,
+    "Sandringham " = 12,
+    // StonyPoint = 13,
+    // Sunbury = 14,
+    // Upfield = 15,
+    // Werribee = 16,  
+    // Williamstown = 17,
+    // ShowgroundsFlemingtonRacecourse = 1482
+}
