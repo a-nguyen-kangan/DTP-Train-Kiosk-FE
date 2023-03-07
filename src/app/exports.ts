21850dcde303fb3fa609cfc576c7c9b463df71d9
Prosthius <mail@callumhopkins.au>
2023-03-08 10:05:11 +1100

    Added direction ID arrays for other relevant train stations

--- src/app/exports.ts
+++ src/app/exports.ts
@@ -19,21 +19,21 @@ export class NextDeparture {
 export enum RouteID {
     "Alamein" = 1,
     "Belgrave" = 2,
-    // Craigieburn = 3,
+    "Craigieburn" = 3,
     "Cranbourne" = 4,
-    // Mernda = 5,
+    "Mernda" = 5,
     "Frankston" = 6,
     "Glen Waverley" = 7,
-    // Hurstbridge = 8,
+    "Hurstbridge" = 8,
     "Lilydale" = 9,
     "Pakenham" = 11,
     "Sandringham" = 12,
-    // StonyPoint = 13,
-    // Sunbury = 14,
-    // Upfield = 15,
-    // Werribee = 16,  
-    // Williamstown = 17,
-    // ShowgroundsFlemingtonRacecourse = 1482
+    "StonyPoint" = 13,
+    "Sunbury" = 14,
+    "Upfield" = 15,
+    "Werribee" = 16,
+    "Williamstown" = 17,
+    "ShowgroundsFlemingtonRacecourse" = 1482
 }
 
 export const directionIDRichmond: object[] = [
@@ -46,4 +46,32 @@ export const directionIDRichmond: object[] = [
     { line: "Pakenham", id: 10 },
     { line: "Sandringham", id: 11 },
     { line: "Flinders Street", id: 1 },
+];
+
+export const directionIDSouthernCross: object[] = [
+    { line: "Alamein", id: 0 },
+    { line: "Belgrave", id: 3 },
+    { line: "Craigieburn", id: 2 },
+    { line: "Cranbourne", id: 4 },
+    { line: "Mernda", id: 9 },
+    { line: "Frankston", id: 5 },
+    { line: "Glen Waverley", id: 6 },
+    { line: "Hurstbridge", id: 7 },
+    { line: "Lilydale", id: 8 },
+    { line: "Pakenham", id: 10 },
+    { line: "Sandringham", id: 11 },
+    { line: "Sunbury", id: 13 },
+    { line: "Upfield", id: 14 },
+    { line: "Werribee", id: 15 },
+    { line: "Williamstown", id: 16 },
+];
+
+export const directionIDEssendon: object[] = [
+    { line: "Craigieburn", id: 2 },
+    { line: "Flinders Street", id: 0 }
+];
+
+export const directionIDBroadmeadows: object[] = [
+    { line: "Craigieburn", id: 2 },
+    { line: "Flinders Street", id: 0 }
 ];
